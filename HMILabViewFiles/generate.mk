a2300b47c03db5372d10339ecead65818fa7b923
Rene van Paassen <rene.vanpaassen@gmail.com>
2025-10-17 14:05:18 +0200

    sanitize mask generation

--- HMILabViewFiles/generate.mk
+++ HMILabViewFiles/generate.mk
@@ -2,6 +2,7 @@
 CNFPATH := $(dir $(realpath $(lastword $(MAKEFILE_LIST))))
 
 MASKFILES = hmilabmaskleft-blur.png hmilabmaskright-blur.png hmilabmaskfront-blur.png
+TESTFILES = hmilabmaskleft-test.png hmilabmaskright-test.png hmilabmaskfront-test.png
 
 ifeq (, $(shell which magick))
 ifeq (, $(shell which convert))
@@ -14,12 +15,16 @@ CONVERT := $(shell which magick)
 endif
 
 MASKOPTS = --export-area-page
+TESTVIEWACTIONS = select-clear; select-by-id:aligner; object-set-attribute:style, display:inline; \
+									select-clear; select-by-id:testbar; object-set-attribute:style, display:inline
+
 
 # default target
 all: ${MASKFILES}
+alltest: ${TESTFILES}
 
 clean:
-	rm -f ${MASKFILES}
+	rm -f ${MASKFILES} ${TESTFILES}
 
 %-blur.png: %-sharp.png
 	${CONVERT} ${<} -channel RGBA -blur 0x5 -gamma 0.45 png32:${@}
@@ -27,3 +32,10 @@ clean:
 %-sharp.png:
 	inkscape --export-filename=${@} \
 		${MASKOPTS} ${CNFPATH}/${@:-sharp.png=.svg}
+
+%-test.png:
+	inkscape --export-filename=${@} \
+		--actions "${TESTVIEWACTIONS}" ${MASKOPTS}  ${CNFPATH}/${@:-test.png=.svg}
+
+%-blur.png: %-sharp.png
+	${CONVERT} ${<} -channel RGBA -blur 0x5 -gamma 0.45 png32:${@}
